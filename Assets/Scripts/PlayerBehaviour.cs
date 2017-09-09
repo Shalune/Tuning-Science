746b5bec4bbe53b525b7ac9ae4d080eb0af6ad2d
Tyler Main <shalune@users.noreply.github.com>
2017-09-09 01:52:35 -0400

    created PlayerController + PlayerBehaviour
    PlayerController - gets input and passes to PlayerBehaviour
    
    PlayerBehaviour - NYI
    
    need to plan structure, may implement humble object design pattern for
    behaviour and controller too

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerBehaviour : MonoBehaviour {
+
+    bool hasJump = false;
+
+    private void Awake()
+    {
+        
+    }
+
+    public void MovePlayer(PlayerController.PlayerInput playerInput)
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerBehaviour))]
+public class PlayerController : MonoBehaviour {
+
+    private PlayerBehaviour behaviour;
+
+    public struct PlayerInput
+    {
+        public Vector3 moveAxes;
+        public bool jump;
+    }
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        behaviour = GetComponent<PlayerBehaviour>();
+    }
+
+    void Update () {
+        ResetInput();
+        GetInput();
+        behaviour.MovePlayer(playerInput);
+	}
+
+    private void ResetInput()
+    {
+        playerInput.moveAxes = Vector3.zero;
+        playerInput.jump = false;
+    }
+
+    private void GetInput()
+    {
+        playerInput.moveAxes.x = Input.GetAxis("Horizontal");
+        playerInput.moveAxes.y = Input.GetAxis("Vertical");
+        if (Input.GetButton("Jump"))
+            playerInput.jump = true;
+    }
+
+    public PlayerBehaviour Behaviour
+    {
+        get { return behaviour; }
+    }
+}
