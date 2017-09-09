65f8453fd63fd76de08a0ffa7482ae55509432fa
Tyler Main <tylermain3049@gmail.com>
2017-09-08 21:28:01 -0400

    initial upload

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump {
+
+    private bool canJump = true;
+
+    public void StartJump()
+    {
+
+    }
+
+    public Vector3 Update()
+    {
+        Vector3 moveBy = Vector3.zero;
+
+        return moveBy;
+    }
+
+    public bool CanJump
+    {
+        get { return canJump; }
+        set { canJump = value; }
+    }
+}
---
+++ Assets/Scripts/JumpMono.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpMono : MonoBehaviour {
+    private Jump jumpScript = null;
+
+    private void Awake()
+    {
+        jumpScript = GetComponent<Jump>() as Jump;
+        if (jumpScript == null)
+            jumpScript = new Jump();
+    }
+
+    public void Jump()
+    {
+        jumpScript.StartJump();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
